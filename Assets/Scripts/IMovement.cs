54bb8c1cafbde4ae891f0ec8049f8599466c41e7
evanspro <156554921+caesium-55@users.noreply.github.com>
2025-10-22 17:23:01 -0600

    working jump, added starter interface, horizontal move randomly stops working

---
+++ Assets/Scripts/IMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IMovement 
+{   
+    //player must implement these functions
+    void Jump();
+    void MoveLeft();
+    void MoveRight();
+    bool IsGrounded();
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,108 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour, IMovement
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private bool wantJump;
+    private float moveInput;
+
+    /////////////////////
+    //private bool wantLeft;
+    //private bool wantRight;
+    /////////////////////
+
+    [Header("Ground Check (Raycast)")]
+    public float groundCheckDistance = 0.1f;
+    public LayerMask groundLayer;
+    public Vector2 rayOffset = new Vector2(0f, 0f);
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {   
+        moveInput = Input.GetAxisRaw("Horizontal");
+        //Debug.Log("rb velocity: " + rb.velocity + ", moveInput: " + moveInput);
+
+
+        //if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            wantJump = true;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        //movement
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // here we put jump in fixedUpdate because rigid body + add force so we don't use the time.delta time thing...
+        if (wantJump)
+        {   
+            Jump();
+            wantJump = false;
+            isGrounded = false;
+        }
+    }
+
+
+    /// <summary>
+    /// this function allow us to visualize the ray in scene :D 
+    /// </summary>
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Vector2 origin = (Vector2)transform.position + rayOffset;
+        Gizmos.DrawLine(origin, origin + Vector2.down * groundCheckDistance);
+    }
+
+
+    ///////////////////////////////
+    public void Jump(){
+        //Debug.Log("called jump: isGrounded? " + isGrounded);
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+
+    public bool IsGrounded(){
+        //check if player can jump right now
+        Vector2 origin = (Vector2)transform.position + rayOffset;
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, groundCheckDistance, groundLayer);
+        isGrounded = (hit.collider != null);
+        return isGrounded;
+    }
+
+
+    public void MoveLeft(){
+        //Debug.Log("called move left: wantLeft? " + wantLeft);
+        //move in negative x direction
+        rb.velocity = new Vector2(-1 * moveSpeed, rb.velocity.y);
+        //wantLeft = false;
+    }
+
+
+    public void MoveRight(){
+        //Debug.Log("called move right: wantRight? " + wantRight);
+        //move in positive x direction
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        //wantRight = false;
+    }
+
+    //////////////////////////////////
+}
+
